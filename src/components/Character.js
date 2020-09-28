e1804ad40d76417e5eeeaa7edd37f344feff236b
Gabriel Henton <gabehenton@gmail.com>
2020-09-27 20:06:24 -0700

    added opacity and text shadows for a cool overall effect

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,20 +5,42 @@ import styled from 'styled-components';
 
 const Character = (props) => {
 
+ 
 const H2 = styled.h2`
-  color: white;
-  background-color: black;
+  color: black;
+  background-color: lightgrey;
+  opacity: 0.5;
   font-size: 1.5rem;
+  text-shadow: -.2rem 0 white, 0 .2rem white, .2rem 0 white, 0 .2rem white;
   margin: .2em;
-  padding: 0.25em .25em;
+  letter-spacing: .5em;
+  padding: 0.5em .5em;
   border: .1rem solid white;
   border-radius: 3px;
 `;
+
+
+/*
+const H2 = styled.h2`
+display: flex;
+flex-wrap: wrap;
+width: 30%;
+justify-content: space-evenly;
+margin-top: 30px;
+align-items: center;
+padding: 20px;
+background-color: gold;
+color: black;
+margin: 5%;
+border-radius: 6px;
+`;
+*/
 
   return (
     <div className="App">
       <H2>{props.name}</H2> 
     </div>
+    
   );
 }
 

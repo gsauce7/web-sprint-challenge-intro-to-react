5f72950f95d1a4bdff706ecc025795336d3ec1dd
Gabriel Henton <gabehenton@gmail.com>
2020-09-27 19:37:54 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
+//styled-components is used on Character.js not here
+import axios from 'axios';
 import './App.css';
 
+//import local components
+import Character from './components/Character'
+
+
+
 const App = () => {
+
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +18,29 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([]);
+  //console.log(data);
+  const fetchData = () =>{
+    axios.get('https://swapi.dev/api/people/')
+      .then(
+        (response)=>{
+          console.log(response.data.results);
+          setData(response.data.results);
+//console.log(data);
+        }
+      )
+  }
+  
+
+useEffect(fetchData, []);
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map((element)=>{
+           return <Character key={element.created} name={element.name} />
+         })} 
+      {console.log(data[0])}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from 'styled-components';
+
+const Character = (props) => {
+
+const H2 = styled.h2`
+  color: white;
+  background-color: black;
+  font-size: 1.5rem;
+  margin: .2em;
+  padding: 0.25em .25em;
+  border: .1rem solid white;
+  border-radius: 3px;
+`;
+
+  return (
+    <div className="App">
+      <H2>{props.name}</H2> 
+    </div>
+  );
+}
+
+export default Character;
